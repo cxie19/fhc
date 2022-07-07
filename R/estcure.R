546b2887d0d90894071120d77b0198e33867b646
cxie19 <90215497+cxie19@users.noreply.github.com>
2022-07-07 09:58:12 -0500

    Update estcure.R

--- R/estcure.R
+++ R/estcure.R
@@ -8,9 +8,12 @@
 #' @export
 #'
 #' @examples
-#' result <- fhcmodel(data=fhc_dat,event_status = "event",event_time="time",,id="id",beta_variable = c("age","sex"),gamma_variable = c("age","sex"),se=F)
-#' estcure(object=result,z_value=c(0,1)) # cure rate for a subgroup population with age as the mean and female
-#' estcure(object=result,z_value=c(-20,0)) # cure rate for a subgroup population with 20 years younger than the mean age and male
+#' result <- fhcmodel(data=fhc_dat,event_status = "event",event_time="time",,id="id",beta_variable = c("age","sex"),
+#'                    gamma_variable = c("age","sex"),se=F)
+#' # cure rate for a subgroup population with age as the mean and female
+#' estcure(object=result,z_value=c(0,1))
+#' # cure rate for a subgroup population with 20 years younger than the mean age and male
+#' estcure(object=result,z_value=c(-20,0))
 
 estcure <- function(object, z_value){
   coef <- object$coef

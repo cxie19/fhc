18e570d4d016614fc8aa6758a6d0d73f7c22879c
cxie19 <90215497+cxie19@users.noreply.github.com>
2022-07-07 09:58:38 -0500

    Update estsurvival.R

--- R/estsurvival.R
+++ R/estsurvival.R
@@ -9,10 +9,14 @@
 #' @export
 #'
 #' @examples
-#' result <- fhcmodel(data=fhc_dat,event_status = "event",event_time="time",id="id",beta_variable = c("age","sex"),gamma_variable = c("age","sex"),se=F)
-#' survival_female <- estsurvival(object=result,z_value=c(mean(fhc_dat$age),1),x_value=c(mean(fhc_dat$age),1),event_time="time")
-#' survival_male <- estsurvival(object=result,z_value=c(mean(fhc_dat$age),0),x_value=c(mean(fhc_dat$age),0),event_time="time")
-#' plot(survival_male$time,survival_male$survival,ylim=c(0,1),xlim=c(0,40),xlab="Time",ylab="Survival probability",type="l",col="red")
+#' result <- fhcmodel(data=fhc_dat,event_status = "event",event_time="time",id="id",
+#'                    beta_variable = c("age","sex"),gamma_variable = c("age","sex"),se=F)
+#' survival_female <- estsurvival(object=result,z_value=c(mean(fhc_dat$age),1),
+#'                                x_value=c(mean(fhc_dat$age),1),event_time="time")
+#' survival_male <- estsurvival(object=result,z_value=c(mean(fhc_dat$age),0),
+#'                              x_value=c(mean(fhc_dat$age),0),event_time="time")
+#' plot(survival_male$time,survival_male$survival,ylim=c(0,1),xlim=c(0,40),xlab="Time",
+#'      ylab="Survival probability",type="l",col="red")
 #' lines(survival_female$time,survival_female$survival,col="blue")
 #' legend(25,1,c("Male","Female"),cex=1,lty=c(1,1),col=c("red","blue"),horiz=F,bty="n")
 
@@ -23,4 +27,3 @@ estsurvival <- function(object,z_value,x_value,event_time){
   est <- exp(-as.vector(exp(coef[1]+coef[2:(1+length(z_value))]%*%z_value))*F_0t^as.vector(exp(coef[(length(coef)-length(x_value)+1):length(coef)]%*%x_value)))
   return(data.frame(time=sort(data[,event_time]),survival=est))
 }
-
